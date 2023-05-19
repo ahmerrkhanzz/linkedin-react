95eb416a7e1d20079a89118aa48e48ad489ee86d
ahmerrkhanzz <ahmerrkhanzz@gmail.com>
2023-05-19 10:46:57 +0500

    basic structure of project

---
+++ firebaseconfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDPl-DpcUnJXuFdricffB-pSqP83uHz7rc",
+  authDomain: "linkedin-clone-e70fc.firebaseapp.com",
+  projectId: "linkedin-clone-e70fc",
+  storageBucket: "linkedin-clone-e70fc.appspot.com",
+  messagingSenderId: "1082444038730",
+  appId: "1:1082444038730:web:18941d17f17920ddab9cb3",
+  measurementId: "G-Z63NKQ1P06"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
